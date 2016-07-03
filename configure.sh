0b0c2458504bd92b7c1b54895b0d53c9aeec17ca
Matt Stasch <matt.stasch@gmail.com>
2016-07-03 20:47:21 +0200

    Setup script added; TMux configuration added

---
+++ configure.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DOTFILES_HOME=`pwd`
+
+echo "Installing dotfiles from ${DOTFILES_HOME}"
+
+# Helper functions
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+error () {
+    (>&2 echo "$1")
+}
+
+if ! command_exists curl ; then
+    echo "Installing curl..."
+    sudo apt-get install -y curl
+fi
+
+if ! command_exists git ; then
+    echo "Installing git..."
+    sudo apt-get install -y git
+fi
+
+if ! command_exists zsh ; then
+    echo "Installing zsh..."
+    sudo apt-get install -y zsh
+fi
+
+echo "Installing oh-my-zsh...(please exit zsh when installation is done!)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>/dev/null
+
+if ! command_exists nvim ; then
+    echo "Installing neovim (and dependencies)..."
+    sudo apt-get install -y software-properties-common &>/dev/null
+    sudo apt-get install -y python-software-properties &>/dev/null
+    sudo apt-get install -y python2 python3 python2-pip python3 pip &>/dev/null
+    sudo python2 -m pip install neovim
+    sudo python3 -m pip install neovim
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install -y neovim
+fi 
+
+echo "Creating symlinks for configuration files..."
+mkdir ~/.config &>/dev/null
+ln -s ${DOTFILES_HOME}/nvim ${HOME}/.config/nvim
+ln -s ${DOTFILES_HOME}/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${DOTFILES_HOME}/.zshrc ${HOME}/.zshrc
--- nvim/init.vim
+++ nvim/init.vim
@@ -2,6 +2,26 @@ syntax on
 set number
 set hidden
 
+set showmatch
+set ruler
+set expandtab
+set tabstop=2
+set textwidth=0
+set shiftwidth=2
+
+" ---------- Splits ---------
+set splitbelow
+set splitright
+
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+nnoremap <C-w>- :split<CR>
+nnoremap <C-w>\| :vsplit<CR>
+" --------------------------
+
 let mapleader = '\'
 
 call plug#begin('~/.config/nvim/plugged')
@@ -35,6 +55,8 @@ map! <leader><leader> <ESC>
 nnoremap <leader><BS> :bdelete<CR>
 nnoremap <leader><leader><BS> :bdelete!<CR>
 nnoremap <leader>] :bnext<CR>
-nnoremap <leader>[ :previous<CR>
+nnoremap <leader>[ :bprevious<CR>
 
+" Save on C-s
 nnoremap <C-s> :w<CR>
+
